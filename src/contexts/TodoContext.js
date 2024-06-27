a494db38bfe033ce4ee01c5455e845fbcec8c343
Piyush kumar <piyushk5531@gmail.com>
2024-06-27 17:22:51 +0530

    first commit

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,28 @@
+import { createContext , useContext } from "react";
+
+export const TodoContext =  createContext({
+    todos:[
+        {
+            id:1,
+            todo:"todo Msg",
+            completed: false
+        },
+        {},
+        { }
+    ]
+    ,
+    addTodo:(todo) =>{ }
+    ,
+    updateTodo: (id,todo)=>{
+
+    },
+    deleteTodo:(todo)=>{},
+    toggleComplete:(id) =>{}
+}
+)
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider;
